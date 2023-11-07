e2d2b34c2003d5c4cfd1f172c90749ba0e4da5ee
sivaprakasaman <andrew.n.sivaprakasam@gmail.com>
2023-11-06 23:07:47 -0500

    making progress...

--- Big_Data_Analyses/figures_forHearingSeminar.R
+++ Big_Data_Analyses/figures_forHearingSeminar.R
@@ -45,18 +45,56 @@ axis(side=1, at=c(0,18,35,60,90), labels=c(0,18,35,60,90))
 
 #plot audiograms
 freqs = c(250,500,1000,2000,4000,8000,16000)
-l_aud_only = rbind(l_data$AC_250,l_data$AC_500,l_data$AC_1000,l_data$AC_2000,l_data$AC_4000,l_data$AC_8000,l_data$AC_1600)
-r_aud_only = rbind(r_data$AC_250,r_data$AC_500,r_data$AC_1000,r_data$AC_2000,r_data$AC_4000,r_data$AC_8000,r_data$AC_1600)
+freq_names = paste0("AC_",freqs)
+  
+l_aud_only = rbind(l_data$AC_250,l_data$AC_500,l_data$AC_1000,l_data$AC_2000,l_data$AC_4000,l_data$AC_8000,l_data$AC_16000)
+l_qs = l_data$QuickSIN;
+r_aud_only = rbind(r_data$AC_250,r_data$AC_500,r_data$AC_1000,r_data$AC_2000,r_data$AC_4000,r_data$AC_8000,r_data$AC_16000)
+r_qs = r_data$QuickSIN;
+age = l_data$Age;
 
 plot_audio <- function(freqs, data_matrix, color = rgb(0,0,1), ylim = c(100,-20), title = 'Audiogram'){
-  matplot(freqs,data_matrix,type = "l", lwd = 2, col = alpha(color, 0.25), lty=1, ylim = ylim, log = 'x',xlab = 'Frequency (Hz)', ylab = "Threshold (dB HL)", main = title, xaxt = "n");
+  matplot(freqs,data_matrix,type = "l", lwd = 3, col = alpha(color, 0.15), lty=1, ylim = ylim, log = 'x',xlab = 'Frequency (Hz)', ylab = "Threshold (dB HL)", main = title, xaxt = "n", yaxt="n");
+  par(new = TRUE)
+  matplot(freqs,rowMeans(data_matrix, na.rm=T),type = "l", lwd = 5, col = color,lty=1, ylim = ylim, log = 'x', xaxt='n', xlab = "n", ylab = "Threshold (dB HL)",yaxt="n");
   axis(side = 1, at = freqs)
   abline(h = seq(-20,100,by=20), col = "gray", lty = 2);
   abline(v = freqs, col = "gray", lty = 2);
 }
 
-plot_audio(freqs, l_aud_only, color = rgb(0,0,1));
-plot_audio(freqs, r_aud_only, color = rgb(1,0,0));
+l_aud_only_t = t(l_aud_only);
+colnames(l_aud_only_t) = freq_names;
+
+r_aud_only_t = t(r_aud_only);
+colnames(r_aud_only_t) = freq_names;
+
+plot_audio(freqs, l_aud_only, color = rgb(0,0,1), title = 'All Left Audios');
+plot_audio(freqs, r_aud_only, color = rgb(1,0,0), title = 'All Right Audios');
+
+#Select only audios with 
+max_thresh = 25;
+
+#return row IDs with 250-8k < 25 dB HL
+rows_nh_r<- which(rowSums(r_aud_only_t[, 1:ncol(r_aud_only_t)-1] < max_thresh) == (ncol(r_aud_only_t) - 1))
+rows_nh_l<- which(rowSums(l_aud_only_t[, 1:ncol(l_aud_only_t)-1] < max_thresh) == (ncol(l_aud_only_t) - 1))
+
+#Trim dataset to normal hearing and hearing loss, I realize rows is actually referring to columns. Only doing this to plot more efficiently
+l_aud_nh = l_aud_only[,rows_nh_l];
+r_aud_nh = r_aud_only[,rows_nh_r];
+
+#loss
+l_aud_hl = l_aud_only[,-rows_nh_l];
+r_aud_hl = r_aud_only[,-rows_nh_r];
+
+color_plt = 'cyan4';
+color_hl = 'brown';
+
+plot_audio(freqs,cbind(l_aud_nh,r_aud_nh),color = color_plt,title = 'Data Grouped by Hearing Status');
+par(new = TRUE)
+plot_audio(freqs,cbind(l_aud_hl,r_aud_hl),color = color_hl, title = '');
+legend('bottomleft',c('Normal Hearing', 'Hearing Loss'),col = c(color_plt,color_hl),lwd=5,cex=1.5,y.intersp=1.25);
+
+
 
 
 setwd(cwd)
